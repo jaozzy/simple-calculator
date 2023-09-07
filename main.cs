2e8b3b905bab3d2b10daa08510830e5afc896a26
João <113316164+jaozzy@users.noreply.github.com>
2023-09-07 16:35:36 -0300

    updated

--- main.cs
+++ main.cs
@@ -1,64 +1,62 @@
 using System;
 
+struct Numeros {
+    public int n1;
+    public int n2;
+}
+
 class Calculadora {
+
+    static Numeros Numbers() {
+
+        Numeros nums;
+
+        Console.Write("Digite o valor de N1:  ");
+        nums.n1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Digite o valor de N2:  ");
+        nums.n2 = Convert.ToInt32(Console.ReadLine());
+
+        return nums;
+
+    }
+
     static void Main() {
 
-        int n1;
-        int n2;
         int r;
         string op;
 
         Console.Write("Qual operação deseja calcular?\n[+]\n[-]\n[*]\n[/]\nR:   ");
         op = Console.ReadLine();
+        
+        Numeros valores = Numbers();
 
-        if (op == "+"){
-            Console.Write("Digite o valor de N1: ");
-            n1 = Convert.ToInt32(Console.ReadLine());
+        switch (op) {
+            case "+":        
+                r = (valores.n1 + valores.n2);
+                Console.Write("O resultado da soma é: " + r);
+                break;
+            case "-":
+                r = (valores.n1 - valores.n2);
+                Console.Write("O resultado da subtração é: " + r);
+                break;
+            case "*":
+                r = (valores.n1 * valores.n2);
+                Console.Write("O resultado da multiplicação é: " + r);
+                break;
+            case "/":
+                if (valores.n2 != 0){
+                    r = (valores.n1 / valores.n2);
+                    Console.Write("O resultado da divisão é: " + r);
+                }
 
-            Console.Write("Digite o valor de N2: ");
-            n2 = Convert.ToInt32(Console.ReadLine());
-
-            r = (n1 + n2);
-
-            Console.Write("A soma de " + n1 + " e " + n2 + " vale: " + r);
-        }
-
-        else if (op == "-") {
-            Console.Write("Digite o valor de N1: ");
-            n1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Digite o valor de N2: ");
-            n2 = Convert.ToInt32(Console.ReadLine());
-
-            r = (n1 - n2);
-
-            Console.Write("A diferença entre " + n1 + " e " + n2 + " vale: " + r);
-            
-        }
-
-        else if (op == "*") {
-            Console.Write("Digite o valor de N1: ");
-            n1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Digite o valor de N2: ");
-            n2 = Convert.ToInt32(Console.ReadLine());
-
-            r = (n1 * n2);
-
-            Console.Write("O produto de " + n1 + " e " + n2 + " vale: " + r);
-        }
-
-        else if (op == "/") {
-            Console.Write("Digite o valor de N1: ");
-            n1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Digite o valor de N2: ");
-            n2 = Convert.ToInt32(Console.ReadLine());
-
-            r = (n1 / n2);
-
-            Console.Write("O quociente da divisão de " + n1 + " por " + n2 + " vale:" + r);
-
+                else {
+                    Console.Write("É impossível dividir por 0.");
+                }
+                break;
+            default:
+                Console.Write("Opção inválida.");
+                break;
         }
 
     }
